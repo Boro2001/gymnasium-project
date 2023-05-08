825ee790196d173f5c1253e8eedb005cc55d64e5
plgborowiczm <mikolajborowicz7@gmail.com>
2023-05-08 16:55:02 +0200

    startup

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 from agent import Agent
+from model import Model
 
 # hyperparameters
 learning_rate = 0.3
@@ -12,109 +13,19 @@ n_episodes = 10000
 start_epsilon = 1.0
 epsilon_decay = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
 final_epsilon = 0.1
-is_slippery=True
+is_slippery = True
 
+# enviroments
 env2 = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=is_slippery, render_mode="human").env
 #env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=True,render_mode="rgb_array").env
 env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=is_slippery,render_mode="ansi").env
 env = gym.wrappers.RecordEpisodeStatistics(env, deque_size=n_episodes)
 
-agent = Agent(
-    env,
-    learning_rate,
-    start_epsilon,
-    epsilon_decay,
-    final_epsilon
-)
-
-observation, info = env.reset()
-#
-for episode in range(n_episodes):
-    obs, info = env.reset()
-    done = False
-    clear_output()
-    print(episode)
-    # play one episode
-    while not done:
-        action = agent.get_action(obs)
-        next_obs, reward, terminated, truncated, info = env.step(action)
-
-        agent.update(obs, action, reward, terminated, next_obs)
-        
-        done = terminated or truncated
-        obs = next_obs
-
-    
-    agent.decay_epsilon()
+model = Model(env)
+model.train_model()
+model.wypisz_ruchy(4)
+model.play_game_without_learning(2)
 
 
-print(agent.q_values)
-
-def wypisz_ruchy(n):
-    
-    for i in range(n):
-        tab = []
-        for j in range(n):
-            index = i*4 + j
-            w = np.argmax(agent.q_values[index])
-            if w == 0: # lewo
-                tab.append('A')
-            if w == 1: # dół
-                tab.append('S')
-            if w == 2: # prawo
-                tab.append('D')
-            if w == 3: # góra
-                tab.append('W')
-
-        print(tab)
-
-wypisz_ruchy(4)
-agent.env = env2
-for episode in range(10):
-    obs, info = env2.reset()
-    done = False
-    clear_output()
-    
-    # play one episode
-    while not done:
-        action = agent.get_action(obs)
-        next_obs, reward, terminated, truncated, info = agent.env.step(action)
-
-        #agent.update(obs, action, reward, terminated, next_obs)
-        
-        done = terminated or truncated
-        obs = next_obs
-
-    
-
-# rolling_length = 500
-# fig, axs = plt.subplots(ncols=3, figsize=(12, 5))
-# axs[0].set_title("Episode rewards")
-# reward_moving_average = (
-#     np.convolve(
-#         np.array(env.return_queue).flatten(), np.ones(rolling_length), mode="valid"
-#     )
-#     / rolling_length
-# )
-# axs[0].plot(range(len(reward_moving_average)), reward_moving_average)
-# axs[1].set_title("Episode lengths")
-# length_moving_average = (
-#     np.convolve(
-#         np.array(env.length_queue).flatten(), np.ones(rolling_length), mode="same"
-#     )
-#     / rolling_length
-# )
-# axs[1].plot(range(len(length_moving_average)), length_moving_average)
-# axs[2].set_title("Training Error")
-# training_error_moving_average = (
-#     np.convolve(np.array(agent.training_error), np.ones(rolling_length), mode="same")
-#     / rolling_length
-# )
-# axs[2].plot(range(len(training_error_moving_average)), training_error_moving_average)
-# plt.tight_layout()
-# plt.show()
-
-env.close()
-env2.close()
 
 
---
+++ model.py
@@ -0,0 +1,88 @@
+import gymnasium as gym
+from gymnasium.envs.toy_text.frozen_lake import generate_random_map
+from IPython.display import clear_output
+from matplotlib import pyplot as plt
+import numpy as np
+import datetime
+
+from agent import Agent
+
+class Model:
+
+    # So model basically contains the agent
+    # and the interface for the learning and playing games
+
+    def __init__(self, env, n_episodes=10000, learning_rate=0.3, start_epsilon=1.0, epsilon_decay=-1,
+                 final_epsilon=0.1):
+        self.n_episodes = n_episodes
+        self.env = env
+        self.agent = Agent(self.env,
+                           learning_rate,
+                           start_epsilon,
+                           epsilon_decay,
+                           final_epsilon)
+        self.obs, self.info = self.env.reset()
+
+    def train_model(self):
+        episode_steps = []  # list of number of steps per episode
+        for episode in range(self.n_episodes):
+            self.obs, self.info = self.env.reset()
+            done = False
+            clear_output()
+            print(episode)
+            # play one episode and get the stats about it
+            number_of_steps = 0
+            while not done:
+                action = self.agent.get_action(self.obs)
+                next_obs, reward, terminated, truncated, info = self.env.step(action)
+                self.agent.update(self.obs, action, reward, terminated, next_obs)
+                print("is terminated " + str(terminated))
+                print("is truncated " + str(truncated))
+                number_of_steps = number_of_steps + 1
+                print("number of steps" + str(number_of_steps))
+                done = terminated or truncated
+                obs = next_obs
+                print(self.agent.q_values)
+            self.agent.decay_epsilon()
+            episode_steps.append(number_of_steps)
+
+        plt.plot(range(self.n_episodes), episode_steps)
+        plt.xlabel("Episode Number")
+        plt.ylabel("Number of Steps")
+        plt.title("Number of Steps per Episode")
+        plt.savefig("charts/" + str(datetime.datetime.now()) + "steps_per_episode.png")
+        plt.show()
+        print(self.agent.q_values)
+
+    def wypisz_ruchy(self, n):
+        for i in range(n):
+            tab = []
+            for j in range(n):
+                index = i * 4 + j
+                w = np.argmax(self.agent.q_values[index])
+                if w == 0:  # lewo
+                    tab.append('A')
+                if w == 1:  # dół
+                    tab.append('S')
+                if w == 2:  # prawo
+                    tab.append('D')
+                if w == 3:  # góra
+                    tab.append('W')
+            print(tab)
+
+    def play_game_without_learning(self, n = 3):
+        self.agent.env = self.env
+        for episode in range(n):
+            obs, info = self.reset()
+            done = False
+            clear_output()
+
+            # play one episode
+            while not done:
+                action = self.agent.get_action(obs)
+                next_obs, reward, terminated, truncated, info = self.agent.env.step(action)
+                done = terminated or truncated
+                obs = next_obs
+
+        self.env.close()
+
